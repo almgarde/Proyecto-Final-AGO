c6df246dc070d39d3207c7687c15b75fb364d3f5
AGO <almagarciadelaobra@iessoterohernandez.es>
2022-04-09 21:07:00 +0200

    Version 1

---
+++ src/main/java/es/iessoterohernandez/ProyectoFinalAGO/ProyectoFinalAgoApplication.java
@@ -0,0 +1,13 @@
+package es.iessoterohernandez.ProyectoFinalAGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalAgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalAgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/iessoterohernandez/ProyectoFinalAGO/ProyectoFinalAgoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iessoterohernandez.ProyectoFinalAGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalAgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
